f2350dffc4742ce75cccd13c5b46f33c236d417b
kariely-lozano <106716385+kariely-lozano@users.noreply.github.com>
2022-07-27 22:14:47 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,13 +1,50 @@
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
 
-using namespace std;
+//Declarciones
 
-extern void menu();
-
-int main (int argc, char const *argv[])
+int input (int cant)
 {
-	menu();
-	
-	return 0;
+  if (cant == 1)
+    {
+      std::cout << "Presione \"Enter\" para lanzar el dado numero 1";
+    }
+  else
+    {
+      std::cout << "Presione \"Enter\" para lanzar el dado numero 2";
+    }
+  std::cin.ignore ();
 }
 
+//Proceso
+
+int Lanzardado ()
+{
+  int ran;
+  srand (time (0));
+  ran = rand () % 6 + 1;
+  std::cout << "El Resultado Del Dado Es " << ran << std::endl;
+   system ("color 03");
+  return ran;
+}
+
+int dado (int pdado, int sdado)
+{
+  std::cout << "Ha avanzado " << pdado + sdado << " Recuadros " << std::endl;
+  return pdado + sdado;
+}
+
+
+
+int main ()
+{
+  int total, primerdado, segundodado;
+
+  input (1);
+  primerdado = Lanzardado ();
+  input (2);
+  segundodado = Lanzardado ();
+  total = dado(primerdado, segundodado);
+  return 0;
+}
